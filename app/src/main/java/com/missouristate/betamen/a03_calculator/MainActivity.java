313a274794bbec3799b066f79d6e8a3c8e3f8220
Pavel <pasha@gsdghrte.com>
2019-09-07 21:48:31 -0500

    Calculator app

---
+++ app/src/main/java/com/missouristate/betamen/a03_calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.missouristate.betamen.a03_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
